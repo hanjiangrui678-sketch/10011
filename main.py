04221df9a104ab79aece9720caa15d66f13d2f4a
hanjiangrui678-sketch <hanjiangrui678@gmail.com>
2026-06-06 21:18:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from wechatpy.client.api import WeChatMessage, WeChatTemplate
 import requests
 import os
 import random
+import urllib.parse
 
 today = datetime.now()
 start_date = os.environ['START_DATE']

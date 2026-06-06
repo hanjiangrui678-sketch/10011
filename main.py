1d129a5768db9a0118494052ade2d330b17f4890
hanjiangrui678-sketch <hanjiangrui678@gmail.com>
2026-06-06 21:21:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,29 +20,40 @@ template_id = os.environ["TEMPLATE_ID"]
 
 
 def get_weather():
-  url = "https://wttr.in/" + urllib.parse.quote(city) + "?format=j1&lang=zh"
+  url = "https://wttr.in/" + urllib.parse.quote(city) + "?format=j1"
   res = requests.get(url).json()
   current = res['current_condition'][0]
-  weather_en = current['weatherDesc'][0]['value']
+  code = int(current['weatherCode'])
   temp = int(current['temp_C'])
   today_forecast = res['weather'][0]
   high = int(today_forecast['maxtempC'])
   low = int(today_forecast['mintempC'])
 
+  # 用国际标准的weatherCode做映射
   weather_map = {
-    "Sunny": "晴", "Clear": "晴",
-    "Partly cloudy": "多云", "Partly Cloudy": "多云",
-    "Cloudy": "阴", "Overcast": "阴",
-    "Mist": "薄雾", "Fog": "雾",
-    "Light rain": "小雨", "Light drizzle": "小雨",
-    "Patchy rain possible": "可能有阵雨",
-    "Moderate rain": "中雨", "Heavy rain": "大雨",
-    "Light snow": "小雪", "Moderate snow": "中雪",
-    "Thunderstorm": "雷阵雨",
+    113: "晴", 116: "多云", 119: "阴", 122: "阴",
+    143: "雾", 176: "小雨", 179: "小雪",
+    182: "雨夹雪", 185: "雨夹雪",
+    200: "雷阵雨", 227: "暴风雪",
+    230: "暴风雪", 248: "雾", 260: "雾",
+    263: "小雨", 266: "小雨", 281: "雨夹雪",
+    284: "雨夹雪", 293: "小雨", 296: "小雨",
+    299: "中雨", 302: "中雨", 305: "大雨",
+    308: "大雨", 311: "雨夹雪", 314: "雨夹雪",
+    317: "雨夹雪", 320: "雨夹雪",
+    323: "小雪", 326: "小雪", 329: "中雪",
+    332: "中雪", 335: "大雪", 338: "大雪",
+    350: "冰雹", 353: "小雨",
+    362: "雨夹雪", 365: "雨夹雪",
+    368: "小雪", 371: "大雪",
+    374: "冰雹", 377: "冰雹",
+    386: "雷阵雨", 389: "雷阵雨",
+    392: "雷阵雨", 395: "大雪",
   }
-  weather_cn = weather_map.get(weather_en, weather_en)
+  weather_cn = weather_map.get(code, "未知")
   weather_desc = weather_cn + "，" + str(low) + "~" + str(high) + "°C"
   return weather_desc, temp
+
 
 
 
